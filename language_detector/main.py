d83b059077b983f05f5872161b769eab3c09e0e4
Dan C Williams <dan.c.williams@gmail.com>
2017-03-24 01:12:59 +0000

    Final Solution

--- language_detector/main.py
+++ language_detector/main.py
@@ -4,5 +4,20 @@
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    word_count = {}
+    
+    list_of_words = text.split()
+    
+    for item in languages:
+        counter = 0
+        for common_word in item['common_words']:
+            if common_word in list_of_words:
+                counter += 1
+        word_count[item['name']]=counter
+    print word_count    
+    
+    maximum = max(word_count, key=word_count.get)
+    
+    print maximum
+            
+    return maximum
